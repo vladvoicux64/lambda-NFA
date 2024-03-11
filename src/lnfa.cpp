ce01a4fbeb2ef81130f51e65ee67644428172149
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-03-11 22:40:56 +0200

    LNFA class implementation.

---
+++ src/lnfa.cpp
@@ -0,0 +1,70 @@
+#include "lnfa.h"
+#include "stack"
+
+
+void LNFA::set_initial_state_id(int initial_state_id)
+{
+    this->initial_state_id_ = initial_state_id;
+}
+
+void LNFA::add_states(const std::vector<int> &new_state_ids)
+{
+    for(auto const &state_id : new_state_ids)
+    {
+        this->states_[state_id] = State();
+    }
+}
+
+void LNFA::add_arcs(const std::vector<std::tuple<int, int, char>> &new_arcs)
+{
+    for(auto const &arc : new_arcs)
+    {
+        this->states_[std::get<0>(arc)].add_outgoing_arc(std::get<2>(arc), &this->states_[std::get<1>(arc)]);
+    }
+}
+
+void LNFA::set_final_states(const std::vector<int> &final_state_ids)
+{
+    for(auto const &state_id : final_state_ids)
+    {
+        this->states_[state_id].set_final();
+    }
+}
+
+void LNFA::clear_state_logs()
+{
+    for(auto &state : this->states_)
+    {
+        state.second.clear_log();
+    }
+}
+
+bool LNFA::test_acceptance(const std::string &word)
+{
+    std::stack<std::pair<State*, size_t>> dfs_stack;
+    size_t word_lenght = word.length();
+    dfs_stack.emplace(&this->states_[this->initial_state_id_], 0);
+    while(!dfs_stack.empty())
+    {
+        State* current_state = dfs_stack.top().first;
+        size_t current_index = dfs_stack.top().second;
+        current_state->log_pass(current_index);
+        dfs_stack.pop();
+        if(current_state->check_final() && current_index == word_lenght)
+        {
+            this->clear_state_logs();
+            return true;
+        }
+        for(const auto &state : current_state->propagate(word[current_index]))
+        {
+            dfs_stack.emplace(state, current_index + 1);
+        }
+        for(const auto &state : current_state->propagate('\0'))
+        {
+            if(current_state->check_loop(current_index))
+            dfs_stack.emplace(state, current_index);
+        }
+    }
+    this->clear_state_logs();
+    return false;
+}
