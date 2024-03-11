2d942c71531fe45af66d441672dff0360e0b4d2a
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-03-11 22:40:33 +0200

    LNFA class header file.

---
+++ include/lnfa.h
@@ -0,0 +1,26 @@
+#ifndef LFA_LAB_LAMBDA_NFA_H
+#define LFA_LAB_LAMBDA_NFA_H
+
+#include "state.h"
+#include "unordered_map"
+#include "vector"
+#include "string"
+#include "tuple"
+
+
+class LNFA
+{
+private:
+    std::unordered_map<int, State> states_;
+    int initial_state_id_ = 0;
+public:
+    void set_initial_state_id(int initial_state_id);
+    void add_states(const std::vector<int> &new_state_ids);
+    void add_arcs(const std::vector<std::tuple<int, int, char>> &new_arcs);
+    void set_final_states(const std::vector<int> &final_state_ids);
+    void clear_state_logs();
+    bool test_acceptance(const std::string &word);
+};
+
+
+#endif //LFA_LAB_LAMBDA_NFA_H
