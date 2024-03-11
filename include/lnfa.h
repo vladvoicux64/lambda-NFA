39a11b9dd721622b237fe649e32527480dc64cc5
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-03-12 00:10:17 +0200

    Made the LNFA::clear_state_logs(0) method private as it is not meant to be user callable.

--- include/lnfa.h
+++ include/lnfa.h
@@ -13,12 +13,12 @@ class LNFA
 private:
     std::unordered_map<int, State> states_;
     int initial_state_id_ = 0;
+    void clear_state_logs();
 public:
     void set_initial_state_id(int initial_state_id);
     void add_states(const std::vector<int> &new_state_ids);
     void add_arcs(const std::vector<std::tuple<int, int, char>> &new_arcs);
     void set_final_states(const std::vector<int> &final_state_ids);
-    void clear_state_logs();
     bool test_acceptance(const std::string &word);
 };
 
